5f1436ebb1a358bca9db0fbd7824a46f65addeac
Jan <janpc01@gmail.com>
2025-01-25 14:10:14 -0700

    Update HelloController.java

--- src/main/java/com/backend/tempo/controllers/HelloController.java
+++ src/main/java/com/backend/tempo/controllers/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @GetMapping("/")
     public String hello() {
-        return "Hello, World!";
+        return "Hello, World! This edit was deployed automatically with Github CI/CD";
     }
 }
