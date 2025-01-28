a95483c81f68850caaadaf8f7e54b56bf9c5f545
janpc01 <janpc01@gmail.com>
2025-01-28 16:40:33 -0700

    Revert to last working commit

--- src/main/java/com/backend/tempo/controllers/HelloController.java
+++ src/main/java/com/backend/tempo/controllers/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @GetMapping("/")
     public String hello() {
-        return "Hello, World! This edit was deployed automatically with Github CI/CD. Checking if loadbalancer is working";
+        return "Hello, World! Git revert";
     }
 }
