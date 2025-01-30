776163c3a18308616d8659a16a91ac542d2ef99c
janpc01 <janpc01@gmail.com>
2025-01-29 17:41:42 -0700

    Created AWSSecretService.java to fetch secrets from AWS Secrets Manager and Created DataSourceConfig.java to configure the database connection

---
+++ src/main/java/com/backend/tempo/config/DataSourceConfig.java
@@ -0,0 +1,38 @@
+package com.backend.tempo.config;
+
+import com.backend.tempo.services.AWSSecretsService;
+import org.json.JSONObject;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        // Fetch secret containing all DB credentials (Modify "POSTGRESQL_SECRET" to your actual secret name)
+        String secretJson = AWSSecretsService.getSecret("POSTGRESQL_SECRET");
+
+        // Parse JSON secret
+        JSONObject secret = new JSONObject(secretJson);
+        String host = secret.getString("POSTGRESQL_HOST");
+        String dbName = secret.getString("POSTGRESQL_DB");
+        String username = secret.getString("POSTGRESQL_USERNAME");
+        String password = secret.getString("POSTGRESQL_PASSWORD");
+
+        // Construct JDBC URL
+        String jdbcUrl = String.format("jdbc:postgresql://%s:%s@%s/%s?sslmode=require", username, password, host, dbName);
+
+        // Configure DataSource
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+}
