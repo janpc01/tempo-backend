567460e3a0f672248516ec885cb5db12da7f4072
janpc01 <janpc01@gmail.com>
2025-01-30 08:52:53 -0700

    Testing without secrets

--- src/main/java/com/backend/tempo/config/DataSourceConfig.java
+++ src/main/java/com/backend/tempo/config/DataSourceConfig.java
@@ -13,26 +13,31 @@ public class DataSourceConfig {
     @Bean
     public DataSource dataSource() {
         // Fetch secret containing all DB credentials (Modify "POSTGRESQL_SECRET" to your actual secret name)
-        String postgresSecret = System.getenv("POSTGRESQL_SECRET");
+        // String postgresSecret = System.getenv("POSTGRESQL_SECRET");
 
         // Parse JSON secret
-        JSONObject secret = new JSONObject(postgresSecret);
-        String host = secret.getString("POSTGRESQL_HOST");
-        String dbName = secret.getString("POSTGRESQL_DB");
-        String username = secret.getString("POSTGRESQL_USER");
-        String password = secret.getString("POSTGRESQL_PASSWORD");
+        // JSONObject secret = new JSONObject(postgresSecret);
+        // String host = secret.getString("POSTGRESQL_HOST");
+        // String dbName = secret.getString("POSTGRESQL_DB");
+        // String username = secret.getString("POSTGRESQL_USER");
+        // String password = secret.getString("POSTGRESQL_PASSWORD");
 
         // Construct JDBC URL
-        String jdbcUrl = String.format("jdbc:postgresql://%s:5432/%s", host, dbName);
+        // String jdbcUrl = String.format("jdbc:postgresql://%s:5432/%s", host, dbName);
         // jdbc:postgresql://<host>:<port>/<dbname>?sslmode=require
 
 
         // Configure DataSource
+        // DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        // dataSource.setDriverClassName("org.postgresql.Driver");
+        // dataSource.setUrl(jdbcUrl);
+        // dataSource.setUsername(username);
+        // dataSource.setPassword(password);
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(jdbcUrl);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
+        dataSource.setUrl("jdbc:postgresql://ep-noisy-waterfall-a4o5vgrn-pooler.us-east-1.aws.neon.tech:5432/neondb");
+        dataSource.setUsername("neondb_owner");
+        dataSource.setPassword("npg_9Fd5sYxlmzWk");
 
         return dataSource;
     }
