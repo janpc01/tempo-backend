94b47b2d79c4eff53cb8668f41864cbef5e04b95
janpc01 <janpc01@gmail.com>
2025-02-08 13:09:30 -0700

    Added datasource to application.properties

--- src/main/java/com/backend/tempo/controllers/HelloController.java
+++ src/main/java/com/backend/tempo/controllers/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @GetMapping("/")
     public String hello() {
-        return "Hello, World! Git revert";
+        return "Hello, World! Added datasource";
     }
 }
